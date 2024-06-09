e22b25ef51d803a62cbf98ada73f147feb195773
Monwar Hossain <monwarhossainmunna@gmail.com>
2024-06-09 13:47:20 +0600

    service section change

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,78 +1,79 @@
 import React from 'react';
-import munna from '../../images/munna.jpg';
+import munna from '../../images/munna.jpeg';
 import Footer from '../Home/Footer/Footer';
 import Zoom from 'react-reveal/Zoom';
 import './About.css';
 const About = () => {
     return (
-        <div  className='container'>
-            <div className='col-md-6' style = {{margin:'0 auto'}}>
-                    <div className='text-center about-container'>
-                        <img src={munna} alt=""/>
-                        <h1 style={{color:'rgb(158, 226, 211)'}}> <span style={{color:'rgb(224, 114, 166)'}}>Monwar</span> Hossain</h1>
-                        <hr style={{width:'300px', border:'2px solid rgb(0, 255, 242)'}}/>
-                        <p className='text-white'>
-                            I am an enthusiastic JavaScript developer with knowing some framework for the frontend and the backend. 
-                            I know Basic JAVA and MySQL but basically a MERN developer. 
-                            I always focus on learning new technology. I am currently studying in the Department of Computer Science and Engineering, Dhaka International University.
-                        </p>
-                    </div>
+        <div className='container'>
+            <div className='col-md-6' style={{ margin: '0 auto' }}>
+                <div className='text-center about-container'>
+                    <img src={munna} alt="" />
+                    <h1 style={{ color: 'rgb(158, 226, 211)' }}> <span style={{ color: 'rgb(224, 114, 166)' }}>Monwar</span> Hossain</h1>
+                    <hr style={{ width: '300px', border: '2px solid rgb(0, 255, 242)' }} />
+                    <p className='text-white'>
+                        I am an enthusiastic JavaScript developer with knowing some framework for the frontend and the backend.
+                        I know Basic JAVA and MySQL but basically a MERN developer.
+                        I always focus on learning new technology. I am Completed My Graduation in the Department of Computer Science and Engineering, Dhaka International University.
+                    </p>
                 </div>
+            </div>
             <div className='p-5 mt-5'>
                 <div className='row'>
-                <Zoom>
-                    <div className='col-md-6'style={{borderRight:'3px solid yellow'}}>
-                        <h1 className='text-center text-white'>My Skills</h1>
-                        <hr className='hr-styled'/>
+                    <Zoom>
+                        <div className='col-md-6' style={{ borderRight: '3px solid yellow' }}>
+                            <h1 className='text-center text-white'>My Skills</h1>
+                            <hr className='hr-styled' />
 
-                        <h2 className='font-colr'>Expertise:</h2>
-                        <div className='technologies'>
-                            <li>JavaScript</li>   
-                            <li>React.js</li>
-                            <li>React Router</li>
-                            <li>ES6</li>
-                            <li>HTML5</li>
-                            <li>CSS3</li>
-                            <li>React Bootstrap</li>
+                            <h2 className='font-colr'>Expertise:</h2>
+                            <div className='technologies'>
+                                <li>JavaScript</li>
+                                <li>React.js</li>
+                                <li>React Router</li>
+                                <li>ES6</li>
+                                <li>HTML5</li>
+                                <li>CSS3</li>
+                                <li>Tailwind CSS</li>
+                                <li>React Bootstrap</li>
+                            </div>
+
+                            <h2 className='font-colr'>Familiar:</h2>
+                            <div className='technologies'>
+                                <li>Redux</li>
+                                <li>Node.js</li>
+                                <li>Express.js</li>
+                                <li>MongoDB</li>
+                            </div>
+
+                            <h2 className='font-colr'>Tools:</h2>
+                            <div className='technologies'>
+                                <li>Visual Studio</li>
+                                <li>Firebase</li>
+                                <li>Netlify</li>
+                                <li>Chrome Dev Tools</li>
+                                <li>Git</li>
+                                <li>Heroku</li>
+                            </div>
                         </div>
 
-                        <h2 className='font-colr'>Familiar:</h2>
-                        <div className='technologies'>
-                            <li>Redux</li>   
-                            <li>Node.js</li>
-                            <li>Express.js</li>
-                            <li>MongoDB</li>
+                        <div className='col-md-5' style={{ marginLeft: '50px' }}>
+                            <h1 className='text-center text-white'>My Education</h1>
+                            <hr style={{ width: '260px', border: '2px solid rgb(0, 255, 242)' }} />
+                            <h2 className='font-colr'>Under Graduation</h2>
+                            <h6 className='text-white'>B.Sc Eng. in Computer Science & Engineering</h6>
+                            <h6 className='text-white'>Dhaka International University</h6>
+                            <p className='text-white'>Session 2018 to 2022</p>
+
+
+                            <h2 className='font-colr'>Diploma Engineering</h2>
+                            <h6 className='text-white'>Diploma in Computer Technology</h6>
+                            <h6 className='text-white'>Cumilla polytechnic Institute</h6>
+                            <p className='text-white'>Session 2013 to 2017</p>
                         </div>
-
-                        <h2 className='font-colr'>Tools:</h2>
-                        <div className='technologies'>
-                            <li>Visual Studio</li>   
-                            <li>Firebase</li>
-                            <li>Netlify</li>
-                            <li>Chrome Dev Tools</li>
-                            <li>Git</li>
-                            <li>Heroku</li>
-                        </div>
-                    </div>
-
-                    <div className='col-md-5' style={{marginLeft:'50px'}}>
-                        <h1 className='text-center text-white'>My Education</h1>
-                        <hr style={{width:'260px', border:'2px solid rgb(0, 255, 242)'}} />
-                        <h2 className='font-colr'>Under Graduation</h2>
-                        <h6 className='text-white'>B.Sc Eng. in Computer Science & Engineering</h6>
-                        <h6 className='text-white'>Dhaka International University</h6>
-                        <p className='text-white'>11th Semester(2021 CURRENT)</p>
-                        
-
-                        <h2 className='font-colr'>Diploma Engineering</h2>
-                        <h6 className='text-white'>Diploma in Computer Technology</h6>
-                        <h6 className='text-white'>Cumilla polytechnic Institute</h6>
-                        <p className='text-white'>Session 2013 to 2017</p>
-                    </div>
                     </Zoom>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 };
--- src/component/Home/Projects/Projects.js
+++ src/component/Home/Projects/Projects.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import libary from '../../../images/laibary.PNG';
-import laptop from '../../../images/laptop.PNG';
-import car from '../../../images/car.PNG';
+import kutumbari from '../../../images/kutumbari.JPG';
+import vehicle from '../../../images/vehicle.JPG';
 import panda from '../../../images/panda.PNG';
 import Zoom from 'react-reveal/Zoom';
 
@@ -15,114 +14,90 @@ const Projects = () => {
     return (
         <div className='container mb-5'>
             <div className='mt-5'>
-                <h2 className='project-h2 text-white'>Some of <span style={{color:'rgb(111, 210, 223)'}}>my project</span> </h2>
+                <h2 className='project-h2 text-white'>Some of <span style={{ color: 'rgb(111, 210, 223)' }}>my project</span> </h2>
             </div>
             <Zoom>
-            <div className='row mb-4'>
-                <div className='col-md-6 project-image'>
-                    <img className='img-fluid rounded' src={laptop} alt=""/>
-                </div>
-                <div className='col-md-6 d-flex align-self-center '>
-                    <div  className='content-left p-4 text-white'>
-                        <h2>Laptop Repair</h2>
-                        <p>
-                            This is a Laptop repair-based website with dashboard. Where people can click buy 
-                            service to get this service. Admin can add any services, manage any services in home 
-                            page and also can see all service order list and change order status and also make 
-                            admin another 
-                        </p>
-                        <div className='technologies mb-2'>
-                            <li>React.js</li>
-                            <li>Express.js</li>
-                            <li>node.js</li>
-                            <li>MongoDB</li>
-                            <li>Stripe</li>
-                            <li>Firebase</li>
-                            <li>Bootstrap</li>
-                        </div>
-                        <div className='social-media'>
-                            <li className="list-inline-item"><a href="https://github.com/MhMunna12/laptop-repair-client"><FontAwesomeIcon className="icon active-icon" icon={faGithub} /></a></li>
-                            <li className="list-inline-item"><a href="https://laptop-repair-ac31e.web.app/"><FontAwesomeIcon className="icon active-icon" icon={faGlobe} /></a></li>
+                <div className='row mb-4'>
+                    <div className='col-md-6 project-image'>
+                        <img className='img-fluid rounded' src={kutumbari} alt="" />
+                    </div>
+                    <div className='col-md-6 d-flex align-self-center '>
+                        <div className='content-left p-4 text-white'>
+                            <h2>Kutumbari Restaurant</h2>
+                            <p>
+                                This is a Kutumbari Restaurant-based website with dashboard. Where people can click buy
+                                the order to get this food service. Admin can add any foods items, manage any food items in home
+                                page and also can see all food order list and change order status and also make
+                                admin another.
+                            </p>
+                            <div className='technologies mb-2'>
+                                <li>React.js</li>
+                                <li>Express.js</li>
+                                <li>node.js</li>
+                                <li>MongoDB</li>
+                                <li>Stripe</li>
+                                <li>Firebase</li>
+                                <li>Tailwind CSS</li>
+                            </div>
+                            <div className='social-media'>
+                                <li className="list-inline-item"><a href="https://github.com/MhMunna12/kutumbari-restaurants-client"><FontAwesomeIcon className="icon active-icon" icon={faGithub} /></a></li>
+                                <li className="list-inline-item"><a href="https://kutumbari-restaurant.web.app//"><FontAwesomeIcon className="icon active-icon" icon={faGlobe} /></a></li>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className='row mb-4'>
-                <div className='col-md-6 project-image'>
-                    <img className='img-fluid rounded' src={libary} alt=""/>
-                </div>
-                <div className='col-md-6  d-flex align-self-center '>
-                    <div className='content-left p-4 text-white'>
-                        <h2>Book Library</h2>
-                        <p>
-                            his is a Book-library e-commerce based website. Where people can click buy product 
-                            to get this service. Admin can add product, manage any product in home page. 
-                        </p>
-                        <div className='technologies mb-2'>
-                            <li>React.js</li>
-                            <li>Express.js</li>
-                            <li>node.js</li>
-                            <li>MongoDB</li>
-                            <li>Firebase</li>
-                            <li>Bootstrap</li>
-                        </div>
-                        <div className='social-media'>
-                            <li className="list-inline-item"><a href="https://github.com/MhMunna12/Book-Libary"><FontAwesomeIcon className="icon active-icon" icon={faGithub} /></a></li>
-                            <li className="list-inline-item"><a href="https://book-library-a7b6e.web.app"><FontAwesomeIcon className="icon active-icon" icon={faGlobe} /></a></li>
+                <div className='row mb-4'>
+                    <div className='col-md-6 project-image'>
+                        <img className='img-fluid rounded' src={vehicle} alt="" />
+                    </div>
+                    <div className='col-md-6  d-flex align-self-center '>
+                        <div className='content-left p-4 text-white'>
+                            <h2>Vehicle Repair</h2>
+                            <p>
+                                This is a Vehicle repair service-center-based website with dashboard. Where people can click a
+                                service to get this service. Admin can add any services, manage any services in home
+                                page and also can see all service.
+                            </p>
+                            <div className='technologies mb-2'>
+                                <li>React.js</li>
+                                <li>Express.js</li>
+                                <li>node.js</li>
+                                <li>MongoDB</li>
+                                <li>Firebase</li>
+                                <li>Tailwind CSS</li>
+                            </div>
+                            <div className='social-media'>
+                                <li className="list-inline-item"><a href="https://github.com/MhMunna12/vehicle-repair-client"><FontAwesomeIcon className="icon active-icon" icon={faGithub} /></a></li>
+                                <li className="list-inline-item"><a href="https://project-fd9f0.web.app"><FontAwesomeIcon className="icon active-icon" icon={faGlobe} /></a></li>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className='row mb-4'>
-                <div className='col-md-6 project-image'>
-                    <img className='img-fluid rounded' src={car} alt=""/>
-                </div>
-                <div className='col-md-6 mb-2 d-flex align-self-center '>
-                    <div className='content-left p-4 text-white'>
-                        <h2>City Service</h2>
-                        <p>
-                            This is a ride car-service website. There some ride option such as bike, car, bus and 
-                            train this ride service users have to click ride option and give some destination 
-                            information and then that ride option will be booked for user. 
-                        </p>
-                        <div className='technologies mb-2'>
-                            <li>React.js</li>
-                            <li>React Router</li>
-                            <li>Bootstrap</li>
-                        </div>
-                        <div className='social-media'>
-                            <li className="list-inline-item"><a href="https://github.com/MhMunna12/Car-service"><FontAwesomeIcon className="icon active-icon" icon={faGithub} /></a></li>
-                            <li className="list-inline-item"><a href="https://city-car-auth.web.app"><FontAwesomeIcon className="icon active-icon" icon={faGlobe} /></a></li>
-                        </div>
-                        
-                    </div>
-                </div>
-            </div>
 
-            <div className='row'>
-                <div className='col-md-6 project-image'>
-                    <img className='img-fluid rounded' src={panda} alt=""/>
-                </div>
-                <div className='col-md-6 mb-2 d-flex align-self-center '>
-                    <div className='content-left p-4 text-white'>
-                        <h2>Panda Commerce</h2>
-                        <p>
-                        A simple , static and one page Ecommerce website design . It is a XD to HTML conversion .
-                        </p>
-                        <div className='technologies mb-2'>
-                            <li>HTML5</li>
-                            <li>CSS3</li>
-                            <li>Firebase</li>
-                            <li>Bootstrap</li>
+                <div className='row'>
+                    <div className='col-md-6 project-image'>
+                        <img className='img-fluid rounded' src={panda} alt="" />
+                    </div>
+                    <div className='col-md-6 mb-2 d-flex align-self-center '>
+                        <div className='content-left p-4 text-white'>
+                            <h2>Panda Commerce</h2>
+                            <p>
+                                A simple , static and one page Ecommerce website design . It is a XD to HTML conversion .
+                            </p>
+                            <div className='technologies mb-2'>
+                                <li>HTML5</li>
+                                <li>CSS3</li>
+                                <li>Firebase</li>
+                                <li>Bootstrap</li>
+                            </div>
+                            <div className='social-media'>
+                                <li className="list-inline-item"><a href="https://github.com/MhMunna12/panda-bootstaro"><FontAwesomeIcon className="icon active-icon" icon={faGithub} /></a></li>
+                                <li className="list-inline-item"><a href="https://mhmunna12.github.io/panda-bootstaro/"><FontAwesomeIcon className="icon active-icon" icon={faGlobe} /></a></li>
+                            </div>
+
                         </div>
-                        <div className='social-media'>
-                            <li className="list-inline-item"><a href="https://github.com/MhMunna12/panda-bootstaro"><FontAwesomeIcon className="icon active-icon" icon={faGithub} /></a></li>
-                            <li className="list-inline-item"><a href="https://mhmunna12.github.io/panda-bootstaro/"><FontAwesomeIcon className="icon active-icon" icon={faGlobe} /></a></li>
-                        </div>
-                        
                     </div>
                 </div>
-            </div>
             </Zoom>
         </div>
     );
